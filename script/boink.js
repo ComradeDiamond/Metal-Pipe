3b78dba5464ee594725ac07f3ccd959c9cd839f5
Justin <JustinChen3946@gmail.com>
2023-05-18 16:56:09 -0400

    Popup images

--- script/boink.js
+++ script/boink.js
@@ -2,7 +2,9 @@ document.addEventListener("keypress", playsound);
 
 function playsound()
 {
-    let sound = new Audio("./soundFiles/metalPipe.mp3");
+    var sfURL = chrome.runtime.getURL("./script/soundFiles/metalPipe.mp3");
+    console.log(sfURL)
+    let sound = new Audio(sfURL);
 
     //Deletes the sound file from global execution context so garbage collection go brr and we don't get blamed for chrome lag
     sound.play().then(() => {
